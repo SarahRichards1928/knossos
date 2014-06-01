8f8767e9c738da316cfb282760906820afbd2cbc
Seba <myeroica@outlook.com>
2014-06-01 21:28:33 +0200

    Added a slideshow effect

--- js/init.js
+++ js/init.js
@@ -1,3 +1,30 @@
+$.stellar();
+
+
+$(function(){
+  $("#slides").slidesjs({
+    navigation: {
+      active: false,
+        // [boolean] Generates next and previous buttons.
+        // You can set to false and use your own buttons.
+        // User defined buttons must have the following:
+        // previous button: class="slidesjs-previous slidesjs-navigation"
+        // next button: class="slidesjs-next slidesjs-navigation"
+      effect: "slide"
+        // [string] Can be either "slide" or "fade".
+    },
+
+    pagination: {
+      active: false,
+        // [boolean] Create pagination items.
+        // You cannot use your own pagination. Sorry.
+      effect: "slide"
+        // [string] Can be either "slide" or "fade".
+    }
+
+  });
+});
+
 /*
 	Twenty 1.0 by HTML5 UP
 	html5up.net | @n33co
